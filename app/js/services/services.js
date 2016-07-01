e4887b69d4a22a829962f31353c8c769ac3c99e2
RexCorpDev <rexcorpfx@gmail.com>
2016-06-30 19:08:11 -0700

    building ng routes

--- app/js/services/services.js
+++ app/js/services/services.js
@@ -11,34 +11,42 @@ angular.module('fullStackTemplate')
 
   this.getProfile = _ => $http.get('/api/users/profile');
 
-})
+});
+angular.module('fullStackTemplate')
 .service('Tenant', function($http){
 
-  this.moveIn   =   moveInObj   =>  $http.post('/api/tenants/move', moveInObj);
+  this.moveIn     =   moveInObj   =>  $http.post('/api/tenants/move', moveInObj);
 
-  this.moveOut  =   moveOutObj  =>  $http.put('/api/tenants/move', moveInObj);
+  this.moveOut    =   moveOutObj  =>  $http.put('/api/tenants/move', moveInObj);
 
+  this.reset      =   ()          =>  $http.delete('/reset');
 
-  this.reset    =   ()          =>  $http.delete('/reset');
+  this.getAll     =   ()          =>  $http.get('/');
+  this.addOne     = newTenant     =>  $http.post('/', newTenant);
+  this.dumpAll    =   ()          =>  $http.delete('/');
 
-  this.getAll   =   ()          =>  $http.get('/');
-  this.getAll   =   ()          =>  $http.get('/');
-
-
-  .get((req, res)     => Tenant.find({}, res.handle))
-  .post((req, res)    =>
-  {
-    console.log('req.body: ', req.body);
-    Tenant.create(req.body, res.handle)
-  })
-  .delete((req, res)  => Tenant.remove({}, res.handle));
-
-  this.route('/:id')
-  .get((req, res)     => Tenant.findById(req.params.id, res.handle))
-  .delete((req, res)  => Tenant.findByIdAndRemove(req.params.id, res.handle))
-  .put((req, res)     => Tenant.findByIdAndUpdate(req.params.id, req.body, {new: true}, res.handle));
+  this.getOne     =   id          =>  $http.get(`/${id}`);
+  this.deleteOne  =   id          =>  $http.delete(`/${id}`);
+  this.editOne    =   id          =>  $http.put(`/${id}`);
 
   module.exports = router;
+});
+angular.module('fullStackTemplate')
+.service('Tenant', function($http){
 
+  this.moveIn     =   moveInObj   =>  $http.post('/api/tenants/move', moveInObj);
 
-})
+  this.moveOut    =   moveOutObj  =>  $http.put('/api/tenants/move', moveInObj);
+
+  this.reset      =   ()          =>  $http.delete('/reset');
+
+  this.getAll     =   ()          =>  $http.get('/');
+  this.addOne     = newTenant     =>  $http.post('/', newTenant);
+  this.dumpAll    =   ()          =>  $http.delete('/');
+
+  this.getOne     =   id          =>  $http.get(`/${id}`);
+  this.deleteOne  =   id          =>  $http.delete(`/${id}`);
+  this.editOne    =   id          =>  $http.put(`/${id}`);
+
+  module.exports = router;
+});
