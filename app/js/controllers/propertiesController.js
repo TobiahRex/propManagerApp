4fc6348cee30ceb87069556d39b44b528a711bb2
RexCorpDev <rexcorpfx@gmail.com>
2016-07-01 12:14:02 -0700

    property controllers in progress

--- app/js/controllers/propertiesController.js
+++ app/js/controllers/propertiesController.js
@@ -32,5 +32,56 @@ angular.module('fullStackTemplate')
     });
   };
 
+  //////////////////////////////////////////////////////////////////////
+  // Add Modal
+  $scope.addProperty = () => {
+    var modalInstance = $uibModal.open({
+      keyboard: true,
+      animation: true,
+      templateUrl: '/uib/template/modal/add-property-modal.html',
+      controller: 'addPropertyModalController',
+      size: 'lg',
+    });
 
+    modalInstance.result.then(function (property) {
+      console.log('property: ', property);
+    }, function (something) {
+      console.log('something: ', something);
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  ////////////////////////////////////////////////////////////////////////
+  /// Edit Modal
+  $scope.editProperty = property => {
+    var modalInstance = $uibModal.open({
+      animation: true,
+      templateUrl: '/uib/template/modal/edit-property-modal.html',
+      controller: 'editPropertyModalController',
+      size: 'lg',
+      resolve : { editProperty : ()=> property }
+    });
+    modalInstance.result.then(function (editedProperty) {
+      console.log('editedProperty: ', editedProperty);
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+  
+  ////////////////////////////////////////////////////////////////////////
+  /// Delete Modal
+  $scope.deleteProperty = property => {
+    var modalInstance = $uibModal.open({
+      animation: true,
+      templateUrl: '/uib/template/modal/delete-property-modal.html',
+      controller: 'deletePropertyModalController',
+      size: 'lg',
+      resolve : { deleteProperty : ()=> property }
+    });
+    modalInstance.result.then(function (deleteProperty) {
+      console.log('deleteProperty: ', deleteProperty);
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
 });
